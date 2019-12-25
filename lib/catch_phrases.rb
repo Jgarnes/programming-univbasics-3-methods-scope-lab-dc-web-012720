ec89d128ae12e77c4406a9155449913ffa5124cf
Jeremy Garnes <j06garnes@gmail.com>
2019-12-25 01:55:04 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,3 +7,13 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link 
+  phrase = "It's Dangerous To Go Alones! Take This."
+  puts phrase
+end 
+
+def any_phrase(phrase)
+  phrase = "Do A Barrel Roll!\n"
+  puts phrase
+end   
